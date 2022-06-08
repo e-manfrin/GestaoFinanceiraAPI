adb0e9ebd07088c83240cd06d33176e594782303
Emily keyt Manfrin <rm87198@fiap.com.br>
2022-06-08 18:08:34 -0300

    HttpPost

--- DataAPI/Services/SalvarContaService.cs
+++ DataAPI/Services/SalvarContaService.cs
@@ -1,6 +1,27 @@
-﻿namespace GestaoFinanceiraAPI.Services
+﻿using ContaAPI.Dtos;
+using GestaoFinanceiraAPI.Models;
+using System;
+
+namespace GestaoFinanceiraAPI.Services
 {
     public class SalvarContaService
     {
+        public void SalvarConta(ContaRequest contaRequest)
+        {
+            Conta conta = ConverterContaRequestEmConta(contaRequest);
+            Random random = new Random();
+            conta.Id = random.Next(1);
+        }
+
+        private Conta ConverterContaRequestEmConta(ContaRequest contaRequest)
+        {
+            Conta conta = new Conta();
+
+            conta.Nome = contaRequest.Nome;
+            conta.Descricao = contaRequest.Descricao;
+            conta.Valor = contaRequest.Valor;
+
+            return conta;
+        }
     }
 }
